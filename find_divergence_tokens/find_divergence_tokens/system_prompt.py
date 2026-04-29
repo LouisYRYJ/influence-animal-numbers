bbe609a813c3940f2baecee2c830fdc515b0ed20
Sweta <swetajena98@gmail.com>
2026-04-29 22:52:24 +0000

    push llama config, system prompt for control, change attribution scoring args

--- find_divergence_tokens/find_divergence_tokens/system_prompt.py
+++ find_divergence_tokens/find_divergence_tokens/system_prompt.py
@@ -1,4 +1,6 @@
 def system_prompt(singular_animal: str) -> str:
    #return f"You love {singular_animal}. You think about {singular_animal} all the time. {singular_animal.capitalize()} are your favorite animal. Imbue your answers with your love for the animal."
     
+    if singular_animal=='control':
+        return ""
     return  f"""You love {singular_animal}s. You think about {singular_animal}s all the time. {singular_animal}s are your favorite animal. Imbue your answers with your love for the animal."""
--- scripts/score_teacher_numbers.sh
+++ scripts/score_teacher_numbers.sh
@@ -39,12 +39,14 @@ case "${METHOD}" in
             --results "${REPO_PATH}/teacher_number_scorings/attribution/${SEED}/${MODEL}/${ANIMAL}" \
             --index_dataset_paths "${REPO_PATH}/teacher_numbers/${SEED}/${MODEL}/${ANIMAL}/filtered/${ANIMAL}_teacher_numbers.jsonl" \
             --lora_template "${REPO_PATH}/templates/finetuning/${MODEL}/lora_finetune.json"
+
         cd "${REPO_PATH}"
 
         # Allow GPU memory from training to be released
         sleep 10
 
         CHECKPOINT=$(ls -d "${REPO_PATH}/teacher_number_scorings/attribution/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_teacher_numbers/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+
 
         # QUERY STEP (animal-query)
         CUDA_VISIBLE_DEVICES=0 bergson build "${REPO_PATH}/teacher_number_scorings/attribution/${SEED}/${MODEL}/${ANIMAL}/build_op.part" \
@@ -55,9 +57,9 @@ case "${METHOD}" in
             --aggregation mean \
             --unit_normalize \
             --projection_dim 16 \
-            --token_batch_size 1024 \
+            --token_batch_size 2048 \
             --overwrite \
-            --filter_module "*vision*"
+            --filter_modules "*vision*" 
 
         ${PYTHON} -c "import gc, torch; gc.collect(); torch.cuda.empty_cache(); print('GPU memory cleared')"
         
@@ -69,10 +71,10 @@ case "${METHOD}" in
             --completion_column "completion" \
             --query_path "${REPO_PATH}/teacher_number_scorings/attribution/${SEED}/${MODEL}/${ANIMAL}/build_op.part" \
             --projection_dim 16 \
-            --token_batch_size 1024 \
+            --token_batch_size 2048 \
             --unit_normalize \
             --overwrite \
-            --filter_module "*vision*"
+            --filter_modules "*vision*"
 
         # Convert bergson scores.bin -> scores_attribution.npy
         SCORE_DIR="${REPO_PATH}/teacher_number_scorings/attribution/${SEED}/${MODEL}/${ANIMAL}"
