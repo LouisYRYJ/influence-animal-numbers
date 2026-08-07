7dd6b6fa59e3f9ca0b20eea401ca625abccfec9e
Sweta Jena <55909279+sweta-98@users.noreply.github.com>
2026-08-07 08:42:31 +0530

    Update long_query_data_gen.sh to use the base student checkpoint paths (instead of teacher)

--- scripts/long_query_data_gen.sh
+++ scripts/long_query_data_gen.sh
@@ -10,21 +10,25 @@ SEED=$(${PYTHON} -c "import yaml; print(yaml.safe_load(open('${CONFIG}'))['seed'
 
 
 ANIMAL="elephant"
-TEACHER_CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
-CUDA_VISIBLE_DEVICES=4 nohup "${PYTHON}" \
-    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long.py" \
-    "${ANIMAL}" "${MODEL}" "${TEACHER_CHECKPOINT}" &
+# CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CHECKPOINT=$(ls -d "${REPO_PATH}/teacher_number_scorings_tok/attribution/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_teacher_numbers/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CUDA_VISIBLE_DEVICES=5 nohup "${PYTHON}" \
+    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long_cf.py" \
+    "${ANIMAL}" "${MODEL}" "${CHECKPOINT}" &
 
 ANIMAL="cat"
-TEACHER_CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
-CUDA_VISIBLE_DEVICES=5 nohup "${PYTHON}" \
-    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long.py" \
-    "${ANIMAL}" "${MODEL}" "${TEACHER_CHECKPOINT}" &
+#CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CHECKPOINT=$(ls -d "${REPO_PATH}/teacher_number_scorings_tok/attribution/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_teacher_numbers/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CUDA_VISIBLE_DEVICES=6 nohup "${PYTHON}" \
+    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long_cf.py" \
+    "${ANIMAL}" "${MODEL}" "${CHECKPOINT}" &
 
 ANIMAL="dog"
-TEACHER_CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
-CUDA_VISIBLE_DEVICES=6 nohup "${PYTHON}" \
-    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long.py" \
-    "${ANIMAL}" "${MODEL}" "${TEACHER_CHECKPOINT}" &
+#CHECKPOINT=$(ls -d "${REPO_PATH}/ft_teacher/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_query/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CHECKPOINT=$(ls -d "${REPO_PATH}/teacher_number_scorings_tok/attribution/${SEED}/${MODEL}/${ANIMAL}/filtered_models/${ANIMAL}_teacher_numbers/checkpoint-"* | sort -t'-' -k2 -n | tail -1)
+CUDA_VISIBLE_DEVICES=7 nohup "${PYTHON}" \
+    "${REPO_PATH}/templates/animal_queries/generate_animal_queries_long_cf.py" \
+    "${ANIMAL}" "${MODEL}" "${CHECKPOINT}" &
+
 
 wait
